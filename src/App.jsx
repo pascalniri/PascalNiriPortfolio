40aab1a3b55978fb10e0e4c59990d0bfb4f921ad
pascalniri <pascalniri@gmail.com>
2025-04-07 12:58:38 +0200

    feat(Home): added the google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,39 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import LandingPage from "./pages/LandingPage";
-import About from "./pages/About"
+import About from "./pages/About";
 import Services from "./components/Services";
 import { useEffect } from "react";
 import { initGA, trackPageView } from "./analytics";
 
-
-
-function App() {
-
+// This component must be inside BrowserRouter to use useLocation
+function TrackPageViews() {
   const location = useLocation();
 
   useEffect(() => {
-    initGA();
-    trackPageView(window.location.pathname);
-  }, [])
+    initGA(); // Initialize GA
+    trackPageView(location.pathname); // Track initial page
+  }, []);
 
   useEffect(() => {
-    trackPageView(location.pathname);
-  }, [location])
-  
+    trackPageView(location.pathname); // Track subsequent navigation
+  }, [location]);
+
+  return null; // This component doesn't render anything
+}
+
+function App() {
   return (
     <BrowserRouter>
+      {/* Tracking component must be inside BrowserRouter */}
+      <TrackPageViews />
+      
       <Navigation />
 
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-
       </Routes>
     </BrowserRouter>
   );
